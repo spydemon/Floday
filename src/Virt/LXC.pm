e203d620f21c94a6db94c2af0a156c53a66ef16b
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2016-05-01 23:28:03 +0200

    [+] #20: update of Virt::LXC lib.

--- src/Virt/LXC.pm
+++ src/Virt/LXC.pm
@@ -1,7 +1,5 @@
 package Virt::LXC;
-
 use v5.20;
-use Data::Dumper;
 
 sub new {
 	my %this;
@@ -24,6 +22,23 @@ sub getStoppedContainers {
 	map{chomp $_; $_} `lxc-ls -1 --stopped`
 }
 
+sub getLxcPath {
+	my ($this) = @_;
+	defined $this->{lxcpath} and return $this->{lxcpath};
+	return "/var/lib/lxc/$this->{utsname}";
+}
+
+sub getConfig {
+	my ($this, $attr) = @_;
+	$this->isExisting or die ('Unexisting container.');
+	open CONF, '<' . $this->getLxcPath . '/config';
+	my @results;
+	for (<CONF>) {
+		/^$attr = (.*)$/ and push @results, $1
+	};
+	return @results;
+}
+
 sub isRunning {
 	my ($this) = @_;
 	defined $this->{'utsname'} or die 'Utsname is missing.';
@@ -41,7 +56,14 @@ sub deploy {
 	defined $this->{'utsname'} or die 'Utsname is missing.';
 	defined $this->{'template'} or die 'Template is missing.';
 	$this->isExisting and die "Container with the $this->{'utsname'} utsname already exists.";
-	`lxc-create -t $this->{'template'} -n $this->{'utsname'} 1>/dev/null`;
+	`lxc-create -t $this->{'template'} -n $this->{'utsname'} 2&1>/dev/null`;
+}
+
+sub destroy {
+	my ($this) = @_;
+	defined $this->{'utsname'} or die 'Utsname is missing.';
+	$this->isRunning and $this->stop;
+	`lxc-destropy -n $this->{'utsname'} 2&1>/dev/null`
 }
 
 sub stop {
@@ -57,9 +79,41 @@ sub start {
 	`lxc-start -n $this->{'utsname'}`;
 }
 
+sub exec {
+	my ($this, $cmd) = @_;
+	$this->isRunning or die 'Can\'t execute something in a non running container.';
+	print `lxc-attach -n $this->{'utsname'} -- $cmd`;
+}
+
 sub setTemplate {
 	my ($this, $templateName) = @_;
 	$this->{template} = $templateName;
 }
 
-0
+sub setLxcPath {
+	my ($this, $path) = @_;
+	$this->{lxcpath} = $path;
+}
+
+sub setConfig {
+	my ($this, $attr, $value) = @_;
+	defined $this->{'utsname'} or die 'Utsname is missing.';
+	$this->isExisting or die "Container $this->{'utsname'} doesn't exist.";
+	my $written = 0;
+	open CONF_R, '<' . $this->getLxcPath . '/config';
+	open CONF_W, '>' . $this->getLxcPath . '/config_r';
+	for (<CONF_R>) {
+		if (/^$attr = .*$/) {
+			print CONF_W "$attr = $value\n";
+			$written = 1;
+		} else {
+			print CONF_W $_;
+		}
+	}
+	!$written and print CONF_W "$attr = $value\n";
+	close CONF_R;
+	close CONF_W;
+	rename $this->getLxcPath. '/config_r', $this->getLxcPath . '/config';
+}
+
+1
