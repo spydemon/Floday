256dd81143e044c8e0b3046ea7ed387e5fc08183
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2016-11-29 19:41:29 +0100

    [-] #28: copy data inside container instance only if 'data_in' and 'data_out' parameters are existing in riuk containers set.

--- containers/riuk/children/core/setups/data.pl
+++ containers/riuk/children/core/setups/data.pl
@@ -3,13 +3,15 @@
 use lib '/opt/floday/src/';
 
 use v5.20;
-use Floday::Setup;
+use Floday::Setup ('ALLOW_UNDEF');
 use Log::Any::Adapter('File', 'log.txt');
 
 my $application = Floday::Setup->new('applicationName' => $ARGV[1]);
 my $lxc = $application->getLxcInstance;
-my $data_in = $application->getParameter('data_in');
-my $data_out = $application->getParameter('data_out');
+my $data_in = $application->getParameter('data_in', ALLOW_UNDEF);
+my $data_out = $application->getParameter('data_out', ALLOW_UNDEF);
 
-$lxc->start if $lxc->isStopped;
-$lxc->put($data_in, $data_out);
+if (defined $data_in && defined $data_out) {
+	$lxc->start if $lxc->isStopped;
+	$lxc->put($data_in, $data_out);
+}
