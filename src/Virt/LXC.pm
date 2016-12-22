c76e9ba051cd42615cfc17a98f710b663478e09c
Kevin Hagner <kevin.hagner@netapsys.fr>
2016-12-22 21:12:31 +0100

    [-] #28: use snake case instead of camel case on lib Virt::LXC.

--- src/Virt/LXC.pm
+++ src/Virt/LXC.pm
@@ -10,24 +10,25 @@ use IPC::Run qw(run);
 
 use constant ALLOW_UNDEF => 0x01;
 our @EXPORT_OK = ('ALLOW_UNDEF');
+our $VERSION = 1.0;
 
 $Backticks::autodie = 1;
 
 has utsname => (
 	'is' => 'ro',
 	'required' => 1,
-	'reader' => 'getUtsname'
+	'reader' => 'get_utsname'
 );
 
 has template => (
 	'is' => 'rw',
-	'reader' => '_getTemplate',
-	'writer' => 'setTemplate'
+	'reader' => '_get_template',
+	'writer' => 'set_template'
 );
 has lxcpath => (
 	'is' => 'rwp',
-	'reader' => '_getLxcPath',
-	'writer'=> 'setLxcPath',
+	'reader' => '_get_lxc_path',
+	'writer'=> 'set_lxc_path',
 );
 
 has log => (
@@ -35,193 +36,192 @@ has log => (
 	default => sub { Log::Any->get_logger },
 );
 
-sub getLxcPath {
+sub get_lxc_path {
 	my ($this) = @_;
-	return $this->_getLxcPath if defined $this->_getLxcPath;
-	'/var/lib/lxc/' . $this->getUtsname;
+	return $this->_get_lxc_path() if defined $this->_get_lxc_path();
+	'/var/lib/lxc/' . $this->get_utsname();
 }
 
-sub getTemplate {
+sub get_template {
 	my ($this) = @_;
-	if (!$this->_getTemplate) {
-		$this->log->errorf('%s: getTemplate: template not provided', $this->getUtsname);
+	if (!$this->_get_template()) {
+		$this->log->errorf('%s: getTemplate: template not provided', $this->get_utsname());
 		croak 'Template is not provided for $this->getUtsname container.';
 	}
-	$this->_getTemplate;
+	$this->_get_template();
 }
 
 sub _qx {
 	my ($this, $cmd, $params, $wantarray) = @_;
-	my $log = $this->getUtsname . ': _qx:`' . $cmd . '`';
+	my $log = $this->get_utsname() . ': _qx:`' . $cmd . '`';
 	$log .= ' => ' . $params if defined $params;
 	$this->log->tracef($log);
 	my @cmd = split(' ', $cmd);
 	my ($stdout, $stderr);
 	my $result = run \@cmd, \$params, \$stdout, \$stderr;
-	$this->log->tracef('%s: _qx res: %s/%s/%s', $this->getUtsname, $result, $stdout, $stderr);
+	$this->log->tracef('%s: _qx res: %s/%s/%s', $this->get_utsname(), $result, $stdout, $stderr);
 	$wantarray and return ($result, $stdout, $stderr);
 	return $result;
 }
 
-sub _checkContainerIsRunning {
+sub _check_container_is_running {
 	my ($this) = @_;
-	if ($this->isStopped) {
+	if ($this->is_stopped()) {
 		my (undef, undef, undef, $caller) = caller(1);
 		$caller =~ /::(\w*)$/;
-		$this->log->errorf('%s: %s: not running', $this->getUtsname, $1);
-		croak 'Container ' . $this->getUtsname . ' is not running';
+		$this->log->errorf('%s: %s: not running', $this->get_utsname(), $1);
+		croak 'Container ' . $this->get_utsname() . ' is not running';
 	}
 }
 
-sub _checkContainerIsExisting {
+sub _check_container_is_existing {
 	my ($this) = @_;
-	if (!$this->isExisting) {
+	if (!$this->is_existing()) {
 		my (undef, undef, undef, $caller) = caller(1);
 		$caller =~ /::(\w*)$/;
-		$this->log->errorf('%s: %s: container not existing', $this->getUtsname, $1);
-		croak 'Container ' . $this->getUtsname . ' doesn\'t exist';
+		$this->log->errorf('%s: %s: container not existing', $this->get_utsname(), $1);
+		croak 'Container ' . $this->get_utsname() . ' doesn\'t exist';
 	}
 }
 
-sub _checkContainerIsNotExisting {
+sub _check_container_is_not_existing {
 	my ($this) = @_;
-	if ($this->isExisting)  {
+	if ($this->is_existing())  {
 		my (undef, undef, undef, $caller) = caller(1);
 		$caller =~ /::(\w*)$/;
-		$this->log->errorf('%s: %s: container alreay exists', $this->getUtsname, $1);
-		croak 'Container ' . $this->getUtsname . ' already exists';
+		$this->log->errorf('%s: %s: container alreay exists', $this->get_utsname(), $1);
+		croak 'Container ' . $this->get_utsname() . ' already exists';
 	}
 }
 
-
-sub getExistingContainers {
+sub get_existing_containers {
 	split("\n", `lxc-ls -1`);
 }
 
-sub getRunningContainers {
+sub get_running_containers {
 	split("\n", `lxc-ls -1 --running`);
 }
 
-sub getStoppedContainers {
+sub get_stopped_containers {
 	split("\n", `lxc-ls -1 --stopped`);
 }
 
-sub getConfig {
+sub get_config {
 	my ($this, $attr, $filter, $flags) = @_;
 	if (defined $filter and ref($filter) ne 'Regexp') {
 		croak '$filter should be a regular expresion';
 	}
 	$filter //= qr/(.*)/;
-	my $allowUndef = defined ($flags) && $flags & ALLOW_UNDEF;
-	$this->_checkContainerIsExisting;
-	open CONF, '<', $this->getLxcPath . '/config';
+	my $allow_undef = defined ($flags) && $flags & ALLOW_UNDEF;
+	$this->_check_container_is_existing();
+	open CONF, '<', $this->get_lxc_path() . '/config';
 	my @results;
 	for (<CONF>) {
 		if (/^$attr\W*=\W*(?P<value>.*)$/) {
 			push @results, $+{value} =~ $filter;
 		}
 	}
-	$this->log->debugf('%s: getConfig \'%s\' with pattern \'%s\': %s', $this->getUtsname, $attr, $filter, \@results);
-	if (!@results && !$allowUndef) {
+	$this->log->debugf('%s: getConfig \'%s\' with pattern \'%s\': %s', $this->get_utsname(), $attr, $filter, \@results);
+	if (!@results && !$allow_undef) {
 		croak "'$attr' attribute was not found in lxc configuration file with filter $filter";
 	}
 	return @results;
 }
 
-sub isRunning {
+sub is_running {
 	my ($this) = @_;
-	my $name = $this->getUtsname;
-	grep {/^$name$/} getRunningContainers;
+	my $name = $this->get_utsname();
+	grep {/^$name$/} get_running_containers();
 }
 
-sub isExisting {
+sub is_existing {
 	my ($this) = @_;
-	my $name = $this->getUtsname;
-	grep {/^$name$/} getExistingContainers;
+	my $name = $this->get_utsname();
+	grep {/^$name$/} get_existing_containers();
 }
 
-sub isStopped {
+sub is_stopped {
 	my ($this) = @_;
-	my $name = $this->getUtsname;
-	grep {/^$name$/} getStoppedContainers;
+	my $name = $this->get_utsname();
+	grep {/^$name$/} get_stopped_containers();
 }
 
 sub deploy {
 	my ($this) = @_;
-	$this->_checkContainerIsNotExisting;
-	my $utsName = $this->getUtsname;
-	my $template = $this->getTemplate;
-	$this->log->infof('%s: deploy', $this->getUtsname);
-	$this->_qx("lxc-create -n $utsName -t $template", undef, wantarray);
-	$this->log->infof('%s: deployed', $this->getUtsname);
+	$this->_check_container_is_not_existing();
+	my $utsname = $this->get_utsname();
+	my $template = $this->get_template();
+	$this->log->infof('%s: deploy', $this->get_utsname());
+	$this->_qx("lxc-create -n $utsname -t $template", undef, wantarray);
+	$this->log->infof('%s: deployed', $this->get_utsname());
 }
 
 sub destroy {
 	my ($this) = @_;
-	$this->isRunning and $this->stop;
-	$this->log->infof('%s: destroy', $this->getUtsname);
-	$this->_qx('lxc-destroy -n '.$this->getUtsname, undef, wantarray);
-	$this->log->infof('%s: destroyed', $this->getUtsname);
+	$this->is_running() and $this->stop();
+	$this->log->infof('%s: destroy', $this->get_utsname());
+	$this->_qx('lxc-destroy -n '.$this->get_utsname(), undef, wantarray);
+	$this->log->infof('%s: destroyed', $this->get_utsname());
 }
 
 sub stop {
 	my ($this) = @_;
-	if (!$this->isRunning) {
-		$this->log->warningf('%s: stop: already stopped', $this->getUtsname);
+	if (!$this->is_running()) {
+		$this->log->warningf('%s: stop: already stopped', $this->get_utsname());
 		return;
 	}
-	$this->log->infof('%s: stop', $this->getUtsname);
-	$this->_qx('lxc-stop -n '.$this->getUtsname, undef, wantarray);
-	$this->log->infof('%s: stopped', $this->getUtsname);
+	$this->log->infof('%s: stop', $this->get_utsname());
+	$this->_qx('lxc-stop -n '.$this->get_utsname(), undef, wantarray);
+	$this->log->infof('%s: stopped', $this->get_utsname());
 }
 
 sub start {
 	my ($this) = @_;
-	my $utsName = $this->getUtsname;
-	$this->_checkContainerIsExisting;
-	if ($this->isRunning) {
-		$this->log->warningf('%s start: already started', $this->getUtsname);
+	my $utsname = $this->get_utsname();
+	$this->_check_container_is_existing();
+	if ($this->is_running()) {
+		$this->log->warningf('%s start: already started', $this->get_utsname());
 		return;
 	}
-	$this->log->infof('%s: start', $this->getUtsname);
-	$this->_qx("lxc-start -d -n $utsName", undef, wantarray);
-	$this->log->infof('%s: started', $this->getUtsname);
+	$this->log->infof('%s: start', $this->get_utsname());
+	$this->_qx("lxc-start -d -n $utsname", undef, wantarray);
+	$this->log->infof('%s: started', $this->get_utsname());
 }
 
 sub exec {
 	my ($this, $cmd) = @_;
-	$this->_checkContainerIsRunning;
-	$this->log->infof('%s: exec `%s`', $this->getUtsname, $cmd);
-	$this->_qx('lxc-attach -n '.$this->getUtsname, $cmd, wantarray);
+	$this->_check_container_is_running();
+	$this->log->infof('%s: exec `%s`', $this->get_utsname(), $cmd);
+	$this->_qx('lxc-attach -n '.$this->get_utsname(), $cmd, wantarray);
 }
 
 sub put {
 	my ($this, $input, $dest) = @_;
-	my ($uid) = $this->getConfig('lxc.id_map', qr/^u 0 (\d+)/);
-	$this->_checkContainerIsExisting;
+	my ($uid) = $this->get_config('lxc.id_map', qr/^u 0 (\d+)/, ALLOW_UNDEF);
+	$this->_check_container_is_existing();
 	if (!-r $input) {
-		$this->log->errorf('%s: put %s: not readable', $this->getUtsname, $input);
+		$this->log->errorf('%s: put %s: not readable', $this->get_utsname(), $input);
 		croak "Input $input is not readable";
 	}
 	if ($dest !~ /^\//) {
-		$this->log->errorf('%s: put %s: destination should be an absolute path', $this->getUtsname, $dest);
+		$this->log->errorf('%s: put %s: destination should be an absolute path', $this->get_utsname(), $dest);
 		croak 'Destination should be an absolute path';
 	}
-	$dest = $this->getLxcPath.'/rootfs'.$dest;
+	$dest = $this->get_lxc_path().'/rootfs'.$dest;
 	$dest =~ /^(.*\/)/;
 	-d $1 or `mkdir -p $1`;
-	$this->log->infof('%s: put: %s on %s', $this->getUtsname, $input, $dest);
+	$this->log->infof('%s: put: %s on %s', $this->get_utsname(), $input, $dest);
 	`cp -R $input $dest`;
-	`chown -R $uid:$uid $dest`;
+	`chown -R $uid:$uid $dest` if defined $uid;
 }
 
-sub setConfig {
+sub set_config {
 	my ($this, $attr, $value) = @_;
-	$this->_checkContainerIsExisting;
-	$this->log->infof('%s: setConfig %s -> %s', $this->getUtsname, $attr, $value);
+	$this->_check_container_is_existing();
+	$this->log->infof('%s: setConfig %s -> %s', $this->get_utsname(), $attr, $value);
 	my $written = 0;
-	open CONF_R, '<' . $this->getLxcPath . '/config';
-	open CONF_W, '>' . $this->getLxcPath . '/config_r';
+	open CONF_R, '<' . $this->get_lxc_path() . '/config';
+	open CONF_W, '>' . $this->get_lxc_path() . '/config_r';
 	for (<CONF_R>) {
 		if (/^$attr = .*$/) {
 			print CONF_W "$attr = $value\n";
@@ -233,7 +233,7 @@ sub setConfig {
 	!$written and print CONF_W "$attr = $value\n";
 	close CONF_R;
 	close CONF_W;
-	rename $this->getLxcPath. '/config_r', $this->getLxcPath . '/config';
+	rename $this->get_lxc_path(). '/config_r', $this->get_lxc_path() . '/config';
 }
 
 1
--- t/integration/lxc.t
+++ t/integration/lxc.t
@@ -9,31 +9,31 @@ use Test::More;
 use Virt::LXC qw(ALLOW_UNDEF);
 
 my $container = Virt::LXC->new(utsname => 'lxc-test');
-$container->setTemplate('flodayalpine -- version 3.4');
-$container->destroy if $container->isExisting;
-ok !$container->isExisting, 'isExisting return false.';
-is $container->getLxcPath, '/var/lib/lxc/lxc-test', 'LxcPath has the good default value.';
-throws_ok {$container->getConfig('lxc.network.ipv4')}
+$container->set_template('sshd');
+$container->destroy if $container->is_existing;
+ok !$container->is_existing, 'is_Existing return false.';
+is $container->get_lxc_path, '/var/lib/lxc/lxc-test', 'Lxcpath has the good default value.';
+throws_ok {$container->get_config('lxc.network.ipv4')}
   qr/Container lxc-test doesn't exist/, 'Error throwed because container is not existing.';
 
 $container->deploy;
-ok $container->isExisting, 'isExisting returns true.';
+ok $container->is_existing, 'is_existing returns true.';
 ok grep{'lxc-test'} `lxc-ls -1`, 'Container is not created.';
-ok grep{'lxc-test'} $container->getExistingContainers, 'Container is present in getExistingContainers.';
-ok grep{'lxc-test'} $container->getStoppedContainers, 'Container is present in getStoppedContainers.';
-ok grep{'lxc-test'} $container->getRunningContainers == 0, 'Container is absent of getRunningContainers.';
-ok !$container->isRunning, 'isRunning returns false.';
-ok $container->isStopped, 'Container is considered as stopped.';
-my @containerConfig = $container->getConfig('lxc.utsname');
+ok grep{'lxc-test'} $container->get_existing_containers, 'Container is present in get_existing_containers.';
+ok grep{'lxc-test'} $container->get_stopped_containers, 'Container is present in get_stopped_containers.';
+ok grep{'lxc-test'} $container->get_running_containers == 0, 'Container is absent of get_running_containers.';
+ok !$container->is_running, 'is_running returns false.';
+ok $container->is_stopped, 'Container is considered as stopped.';
+my @containerConfig = $container->get_config('lxc.utsname');
 is $containerConfig[0], 'lxc-test', 'Can fetch a configuration value.';
-is_deeply {$container->getConfig('lxc.id_map', qr/u (\d+) \d+ (\d+)/)},
-	{0, 100000}, 'Check multi select configuration value.';
+is_deeply {$container->get_config('lxc.mount.entry', qr/lib (\w+) none (.*?) /)},
+	{'lib', 'ro,bind'}, 'Check multi select configuration value.';
 
 $container->start;
-ok !$container->isStopped, 'Container is not considered as stopped.';
-ok grep{'lxc-test'} $container->getStoppedContainers == 0, 'Container is not present is getStoppedContainers.';
-ok $container->isRunning, 'Container is considered as running.';
-ok grep{'lxc-test'} $container->getRunningContainers, 'Container is present in getRunningContainers.';
+ok !$container->is_stopped, 'Container is not considered as stopped.';
+ok grep{'lxc-test'} $container->get_stopped_containers == 0, 'Container is not present is get_stopped_containers.';
+ok $container->is_running, 'Container is considered as running.';
+ok grep{'lxc-test'} $container->get_running_containers, 'Container is present in get_running_containers.';
 
 `echo testing > /tmp/lxc-test.txt`;
 $container->put('/tmp/lxc-test.txt', '/etc/random/lxc-test.txt');
@@ -46,26 +46,24 @@ ok (chomp $stdout) eq 'testing', 'Stdout return of exec is correct.';
 ok $stderr eq '', 'Stderr of exec is correct.';
 
 $container->stop;
-ok $container->isStopped, 'Container is stopped';
+ok $container->is_stopped, 'Container is stopped';
 
-$container->setConfig('newnode', '42');
-my @configValues = $container->getConfig('newnode');
+$container->set_config('newnode', '42');
+my @configValues = $container->get_config('newnode');
 is $configValues[0], '42', 'Creation of a new configuration attribute.';
-$container->setConfig('lxc.network.ipv4', '42.42.42.42');
-@configValues = $container->getConfig('lxc.network.ipv4');
+$container->set_config('lxc.network.ipv4', '42.42.42.42');
+@configValues = $container->get_config('lxc.network.ipv4');
 is $configValues[0], '42.42.42.42', 'Update of a configuration attribute.';
-my ($sequenceFilterTest) = $container->getConfig('lxc.id_map', qr/^u 0 (\d+) .*$/);
-ok grep{/^\d+$/} $sequenceFilterTest, 'Fetching configuration with filter.';
-throws_ok {$container->getConfig('lxc.non-existing')}
+throws_ok {$container->get_config('lxc.non-existing')}
   qr/'lxc.non-existing' attribute was not found in lxc configuration file with filter (?^u:(.*))/;
-is $container->getConfig('lxc.non-existing', undef, ALLOW_UNDEF), 0, 'GetConfig with ALLOW_UNDEF';
+is $container->get_config('lxc.non-existing', undef, ALLOW_UNDEF), 0, 'get_config with ALLOW_UNDEF';
 
 $container->destroy;
-ok grep{'lxc-test'} $container->getExistingContainers == 0, 'Container is absent of getExistingContainers.';
+ok grep{'lxc-test'} $container->get_existing_containers == 0, 'Container is absent of get_existing_containers.';
 ok grep{'lxc-test'} `lxc-ls -1` == 0, 'Container doesn\'t exist anymore.';
 
-$container->setLxcPath('/tmp/randomdest/container');
-is $container->getLxcPath, '/tmp/randomdest/container', 'New container path was assigned.';
+$container->set_lxc_path('/tmp/randomdest/container');
+is $container->get_lxc_path, '/tmp/randomdest/container', 'New container path was assigned.';
 
 TODO: {
 	local $TODO = 'Creation of container somewhere else but the default location is not supported yet.';
