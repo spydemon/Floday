914584e50ccc2c09d6a31ffdf7bac279911ebab8
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2017-01-10 20:02:35 +0100

    [+] #28: manage colors and indentation in the logger.

--- src/Floday/Helper/Logging.pm
+++ src/Floday/Helper/Logging.pm
@@ -8,6 +8,48 @@ use Log::Any::Adapter::Base;
 use Log::Any::Adapter::Util qw(logging_methods detection_methods numeric_level);
 our @ISA = qw(Log::Any::Adapter::Base);
 
+use Term::ANSIColor;
+use Unix::Syslog qw{:macros :subs};
+
+my %SYSLOG_PRIORITY_MAPPER = (
+  trace     => LOG_DEBUG,
+  debug     => LOG_DEBUG,
+  info      => LOG_INFO,
+  inform    => LOG_INFO,
+  notice    => LOG_NOTICE,
+  warning   => LOG_WARNING,
+  warn      => LOG_WARNING,
+  error     => LOG_ERR,
+  err       => LOG_ERR,
+  critical  => LOG_CRIT,
+  crit      => LOG_CRIT,
+  fatal     => LOG_CRIT,
+  alert     => LOG_ALERT,
+  emergency => LOG_EMERG,
+);
+
+my %COLOR_PRIORITY_MAPPER = (
+  trace     => 'bright_cyan',
+  debug     => 'bright_cyan',
+  info      => 'yellow',
+  inform    => 'yellow',
+  notice    => 'green',
+  warning   => 'yellow',
+  warn      => 'yellow',
+  error     => 'red',
+  err       => 'red',
+  critical  => 'red',
+  crit      => 'red',
+  fatal     => 'bold red',
+  alert     => 'bold red',
+  emergency => 'bold red'
+);
+
+sub get_indent {
+	my ($self) =@_;
+	$self->{indent} // 0;
+}
+
 sub init {
 	my ($self) = @_;
 	#I don't know how to set this log_level from the adapter initialisation… If you know the answer, please write me an email. ^^"
@@ -19,8 +61,11 @@ sub init {
 foreach my $method (logging_methods()) {
 	no strict 'refs';
 	*{$method} = sub {
-		my ($self, $text) = @_;
-		say STDOUT $text;
+		my $self = shift @_;
+		my $text = join(' ', @_);
+		$text = '•' x get_indent($self) . ' ' . $text;
+		say STDOUT colored($text, $COLOR_PRIORITY_MAPPER{$method});
+		syslog($SYSLOG_PRIORITY_MAPPER{$method}, '%s', $text);
 	}
 }
 
--- src/test.pl
+++ src/test.pl
@@ -7,6 +7,9 @@ use warnings;
 use Log::Any::Adapter('+Floday::Helper::Logging');
 use Log::Any;
 
+use Data::Dumper;
+
 my $logger = Log::Any->get_logger('category', 'test', 'log_level', 'tracefuck');
-$logger->debugf('It is just a test %s.', 'oz');
+$logger->{adapter}{indent} = 4;
+$logger->tracef('It is just a test %s.', 'oz');
 
